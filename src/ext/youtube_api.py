4b1b2693168a68edae72721c06e0b2fba0af6688
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-10-31 13:21:35 -0200

    Creating API for youtube data

---
+++ src/ext/youtube_api.py
@@ -0,0 +1,46 @@
+from extractor import Extractor, GameNotFound, PageNotFound
+import requests
+import json
+
+class YoutubeAPI(Extractor):
+
+	API_KEY = 'AIzaSyD7uEK8S8NE79iHUx70rTvwXBCEHp5BosQ'
+
+	def get_game(self, identifier):
+		videos = self.get_videos(identifier)
+		if len(videos) != 0:
+			result = self.manipulate_data(videos)
+			return result
+		else:
+			raise GameNotFound("Jogo não encontrado!!!")
+
+
+	def get_videos(self, identifier):	
+		videos_id = []
+		url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q={}&type=video&key={}&order=viewCount'
+		response = self.get_api(url, identifier)
+		for vid in response['items']:
+			videos_id.append(vid['id']['videoId'])
+		return videos_id
+
+	def get_api(self, url, identifier):
+		response = requests.get(url.format(identifier, self.API_KEY))
+		if response.status_code == requests.codes.ok:
+			response = json.loads(response.text)
+			return response
+		else:
+			raise PageNotFound("Página não encontrada!!!")
+
+	def manipulate_data(self, data):
+		result = {'view_count': 0, 'like_count': 0, 'dislike_count': 0}
+		url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id={}&key={}'
+		for vid in data:
+			response = self.get_api(url, vid)['items'][0]['statistics']
+			result['view_count'] += int(response['viewCount'])
+			result['like_count'] += int(response['likeCount'])
+			result['dislike_count'] += int(response['dislikeCount'])
+			return result
+
+		
+a = YoutubeAPI()
+print(a.get_game('Don\'t Starve'))
