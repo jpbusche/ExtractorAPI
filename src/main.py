2aa9e11546b5f213a64027252feeffca8faab000
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-10-31 16:54:43 -0200

    Starting use colorlog

--- src/main.py
+++ src/main.py
@@ -2,13 +2,26 @@ from ext.currency import Currency
 from ext.steam_api import SteamAPI
 from ext.steam_spy import SteamSpy
 from db.elastic import Elastic
+import colorlog
 
-elastic = Elastic('elastic:9200', 'test')
-id = input("Entre com o número de ID: ")
-stapi = SteamAPI()
-stspy = SteamSpy()
-curr = Currency()
-game = stapi.get_game(id)
-game.update(stspy.get_game(id))
-game.update(curr.get_game(id))
-elastic.update(id, game)
+def setup_logger():
+	formatter = colorlog.ColoredFormatter(
+		"%(asctime)s -- %(log_color)s %(levelname)s: %(message)s",
+		datefmt = '%d/%m/%y|%H:%M:%S',
+		reset = True,
+		log_colors = {
+			'DEBUG': 'cyan',
+			'INFO': 'green',
+			'WARNING': 'purple',
+			'ERROR': 'red',
+			'CRITICAL': 'bold_red'
+		}
+	)
+	handler = colorlog.StreamHandler()
+	handler.setFormatter(formatter)
+	logger = colorlog.getLogger('Extractor')
+	logger.addHandler(handler)
+	logger.setLevel('DEBUG')
+	return logger
+
+log = setup_logger()
