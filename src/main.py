f13fb83029f31cd647a1a3149163359e52bf4a80
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-11-07 22:32:53 -0200

    Treating the errors

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from ext.youtube_api import YoutubeAPI
 from ext.extractor import GameNotFound, PageNotFound
 from db.elastic import Elastic
 from utils import setup_logger, get_all_games
+import time
+import os
 
 steam_api = SteamAPI()
 steam_spy = SteamSpy()
@@ -13,6 +15,7 @@ youtube_api = YoutubeAPI()
 
 log = setup_logger()
 log.info('Initializing Elasticsearch')
+fail_id = open("ids_fails.txt", "a")
 try:
 	elastic = Elastic('elastic:9200', 'steam')
 	elastic.delete_index()
@@ -34,7 +37,36 @@ try:
 			elastic.update(game_id, game)
 			log.info('Finishing insersion in the Elasticsearch')
 		except Exception as error:
-			log.error(error)
+			if type(error) == GameNotFound:
+				log.warning(error)
+			else:
+				log.error(error)
+			fail_id.write(str(game_id) + " " + str(game_name))
+	time.sleep(20*60)
+	ids = open("ids_fails.txt", "r")
+	log.info("Starting extraction of the fail ids")
+	for i in ids:
+		game_id, game_name = i.split(" ")
+		game_id = int(game_id)
+		log.info('Re-Starting the extraction of game: %s - %s', game_id, game_name)
+		try:
+			game = steam_api.get_game(game_id)
+			log.info('Steam API: successed!')
+			game.update(steam_spy.get_game(game_id))
+			log.info('Steam SPY: successed!')
+			game.update(steam_currency.get_game(game_id))
+			log.info('Steam Currency: successed!')
+			game.update(youtube_api.get_game(game_name))
+			log.info('Youtube API: successed!')
+			log.info('Starting insersion in the Elasticsearch')
+			elastic.update(game_id, game)
+			log.info('Finishing insersion in the Elasticsearch')
+		except Exception as error:
+			if type(error) == GameNotFound:
+				log.warning(error)
+			else:
+				log.error(error)
+	os.remove("ids_fails.txt")
+
 except Exception as error:
 	log.error(error)
-	
