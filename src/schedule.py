4dae66cd78bed01abc97e5c4ac740d126d8fd9d8
João Paulo Busche <joaopaulo.busche@gmail.com>
2018-12-11 13:54:51 -0200

    Changing crontab

--- src/schedule.py
+++ src/schedule.py
@@ -17,14 +17,13 @@ steam_currency = Currency()
 youtube_api = YoutubeAPI()
 app.conf.broker_url = 'redis://redis:6379/0'
 log = setup_logger()
-up_games = get_games_db()
 
 @app.task
 def insert_new_games():
 	elastic = Elastic('elastic:9200', 'steam_est')
 	log.info('Insert new games on Elasticsearch!')
 	fail_id = open("ids_fails.txt", "a")
-	lst1 = elastic.get_all()
+	lst1 = get_games_db()
 	lst2 = get_all_games()
 	games = [game for game in lst2 if game not in lst1]
 	for game in games:
@@ -51,7 +50,8 @@ def insert_new_games():
 def update_steam_api():
 	elastic = Elastic('elastic:9200', 'steam_tmp')
 	log.info('Updating data from Steam API!')
-	for game in up_games:
+	games = get_games_db()
+	for game in games:
 		log.info('Starting the extraction of game: %s - %s', game[0], game[1])
 		try:
 			gm = steam_api.get_game(int(game[0]), 'temporal')
@@ -71,7 +71,8 @@ def update_steam_api():
 def update_steam_spy():
 	elastic = Elastic('elastic:9200', 'steam_tmp')
 	log.info('Updating data from Steamspy API!')
-	for game in up_games:
+	games = get_games_db()
+	for game in games:
 		log.info('Starting the extraction of game: %s - %s', game[0], game[1])
 		try:
 			gm = steam_spy.get_game(int(game[0]), 'temporal')
@@ -90,7 +91,8 @@ def update_steam_spy():
 def update_steam_currency():
 	elastic = Elastic('elastic:9200', 'steam_tmp')
 	log.info('Updating data from Steam Currency!')
-	for game in up_games:
+	games = get_games_db()
+	for game in games:
 		log.info('Starting the extraction of game: %s - %s', game[0], game[1])
 		try:
 			gm = steam_currency.get_game(int(game[0]), 'temporal')
@@ -110,7 +112,8 @@ def update_steam_currency():
 def update_youtube_api():
 	elastic = Elastic('elastic:9200', 'steam_tmp')
 	log.info('Updating data from Youtube API!')
-	for game in up_games:
+	games = get_games_db()
+	for game in games:
 		log.info('Starting the extraction of game: %s - %s', game[0], game[1])
 		try:
 			gm = youtube_api.get_game(str(game[1]), 'temporal')
@@ -155,19 +158,19 @@ def try_fails_id():
 app.conf.beat_schedule = {
 	"update-steam-api": {
 		"task": "schedule.update_steam_api",
-		"schedule": crontab(minute=0, hour=0)
+		"schedule": crontab(minute=0, hour=8)
 	},
 	"update-steamspy-api": {
 		"task": "schedule.update_steam_spy",
-		"schedule": crontab(minute=0, hour=0)
+		"schedule": crontab(minute=0, hour=8)
 	},
 	"update-currency-api": {
 		"task": "schedule.update_steam_currency",
-		"schedule": crontab(minute=0, hour=0)
+		"schedule": crontab(minute=0, hour=8)
 	},
 	"update-youtube-api": {
 		"task": "schedule.update_youtube_api",
-		"schedule": crontab(minute=0, hour=0)
+		"schedule": crontab(minute=0, hour=8)
 	},
 	"insert-new-games": {
 		"task": "schedule.insert_new_games",
--- src/utils.py
+++ src/utils.py
@@ -37,12 +37,15 @@ def get_all_games():
 
 def get_games_db():
 	games = []
-	response = requests.get('http://elastic:9200/steam_est/_search?')
+	response = requests.get('http://game-elt.ml:9200/steam_est/_search?')
 	response = json.loads(response.content)
 	total = response['hits']['total']
-	response = requests.get('http://elastic:9200/steam_est/_search?size=' + str(total))
-	response = json.loads(response.content)['hits']['hits']
+	response = requests.get('http://game-elt.ml:9200/steam_est/_search?size=' + str(total))
+	response = json.loads(response.content)
+	games = response['hits']['hits']
 	for game in response:
 		pair = (game['_source']['steam_id'], game['_source']['name'])
 		games.append(pair)
 	return games
+
+get_games_db()
